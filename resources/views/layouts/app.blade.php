1b1cfa08e047d2dfc36aeb509d19a05fc83c8c81
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-08-12 23:06:39 +0700

    template inheeritence

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="h-full">
+    <div class="min-h-full">
+        @include('partials.navbar')
+
+
+        <header class="relative bg-white shadow-sm">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">@yield('title')</h1>
+            </div>
+        </header>
+
+        <main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                @yield('content')
+            </div>
+        </main>
+    </div>
+
+</body>
+
+</html>
