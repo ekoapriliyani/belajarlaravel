966526e4141ebe467edd6a9f833eacc03ceb4974
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-08-18 14:11:05 +0700

    posts

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,25 @@ Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    $posts = [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Eko Apriliyani',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum reprehenderit neque
+            illum rem veritatis,
+            consequuntur modi, inventore aliquam atque vel perferendis at. Beatae natus possimus, consequatur tempore
+            labore voluptates harum.'
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'author' => 'Mareko Sanjaya',
+            'body' => 'manda jaus lorem ipsum to dor,
+            consequuntur modi, inventore aliquam atque vel perferendis at. Beatae natus possimus, consequatur tempore
+            labore voluptates harum.'
+        ]
+    ];
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/about', function () {
