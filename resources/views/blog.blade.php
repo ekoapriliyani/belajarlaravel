9d6d77971b3d0bb5f315272f6ef6cba4838a77f1
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-08-18 10:29:44 +0700

    blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,3 +1,12 @@
 <x-layout :title="$title">
-    <p>Welcome to blog page</p>
+    <article>
+        <h2>Judul Artikel 1</h2>
+        <div>
+            <a href="">Eko Apriliyani</a> | 1 Januari 2025
+        </div>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum reprehenderit neque illum rem veritatis,
+            consequuntur modi, inventore aliquam atque vel perferendis at. Beatae natus possimus, consequatur tempore
+            labore voluptates harum.</p>
+        <a href="">Read More &raquo;</a>
+    </article>
 </x-layout>
