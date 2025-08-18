2d60d8b75eb461565c22dd683b7cebed02e49c0f
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-08-18 15:14:22 +0700

    add post feature

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +10,8 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     $posts = [
         [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Eko Apriliyani',
             'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum reprehenderit neque
@@ -17,6 +20,8 @@ Route::get('/posts', function () {
             labore voluptates harum.'
         ],
         [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Mareko Sanjaya',
             'body' => 'manda jaus lorem ipsum to dor,
@@ -25,6 +30,35 @@ Route::get('/posts', function () {
         ]
     ];
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+Route::get('/posts/{id}', function ($id) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Eko Apriliyani',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum reprehenderit neque
+            illum rem veritatis,
+            consequuntur modi, inventore aliquam atque vel perferendis at. Beatae natus possimus, consequatur tempore
+            labore voluptates harum.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Mareko Sanjaya',
+            'body' => 'manda jaus lorem ipsum to dor,
+            consequuntur modi, inventore aliquam atque vel perferendis at. Beatae natus possimus, consequatur tempore
+            labore voluptates harum.'
+        ]
+    ];
+    $post = Arr::first($posts, function ($post) use ($id) {
+        return $post['id'] == $id;
+    });
+    if (!$post) abort(404);
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/about', function () {
