e65e4b33c661223d710984bd7a4ad304f670ac72
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-08-21 08:13:53 +0700

    post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,10 @@
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
             </a>
             <div class="text-base text-gray-500">
-                <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | 1
+                By <a href="/authors/{{ $post->author->username }}"
+                    class="text-gray-900 hover:underline">{{ $post->author->name }}</a>
+                in <a href="/categories/{{ $post->category->slug }}"
+                    class="text-gray-900 hover:underline">{{ $post->category->name }}</a> | 1
                 Januari 2025
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
