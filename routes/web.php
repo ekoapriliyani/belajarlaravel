34a383a4e4c7f7c3371efcfdd9e29537110c75b9
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-08-24 21:02:12 +0700

    prevent lazy loading

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
+    // $posts = Post::with(['author', 'category'])->latest()->get();
     $posts = Post::latest()->get();
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
@@ -22,10 +23,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 // route model binding => {user:username}
 Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load('category', 'author');
     return view('posts', ['title' => count($user->posts) . ' Article by. ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+    // $posts = $category->posts->load('category', 'author');
     return view('posts', ['title' => 'Category: ' . $category->name, 'posts' => $category->posts]);
 });
 
